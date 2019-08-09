b870c0274bb92db75d8360a158334d6d6b03ca71
mayuan1 <43050290+mayuan1@users.noreply.github.com>
2019-08-08 21:57:43 -0400

    version 2

--- v1.py
+++ v1.py
@@ -30,7 +30,10 @@ cols = ['game_name', 'language', 'delay_setting', 'playback_bitrate', 'source_re
 df = df[df.game_name != '-1']
 df = df[df.language != '-1']
 df = df[df.playback_bitrate != -1]
-df = df[df.current_views > 100]
+# df = df[df.current_views > 100]
+
+
+
 
 # for col in cols:
 #     df['Name'] = df[col]
@@ -41,9 +44,11 @@ df = df[df.current_views > 100]
 #     sns.barplot(x=col, y='Name', data=chart[:30], palette=sns.cubehelix_palette((12 if col == 'Genre' else 30), dark=0.3, light=.85, reverse=True)).set_title(('Game count by '+col), fontsize=16)
 #     plt.ylabel('Count', fontsize=14)
 #     plt.xlabel('')
+#     #plt.xticks(rotation=45) 
+#     plt.tight_layout()
 #     plt.show()
 
-# df = df[df.current_views > 5]
+df = df[df.current_views > 5]
 
 # for col in cols:
 #     df['Name'] = df[col]
@@ -61,7 +66,7 @@ df['broadcasters_created_time'] = df['broadcasters_created_time'].astype(str).st
 df[['broadcasters_created_time']] = df[['broadcasters_created_time']].apply(pd.to_numeric)
 df['source_resolution'] = df['source_resolution'].map(lambda x: re.sub(r'\[a-z]+', '', x))
 
-print(df[:5])
+# print(df[:5])
 
 def game_rename(name):
     if name == "League of Legends":
@@ -72,6 +77,8 @@ def game_rename(name):
         return 'StarCraft II'
     elif name ==  "World of Warcraft: Warlords of Draenor":
         return 'WOW'
+    elif name ==  "Call of Duty: Advanced Warfare":
+        return 'COD:AW'
     elif name ==  "Counter-Strike: Global Offensive":
         return 'CS: GO'
     else:
@@ -104,16 +111,17 @@ df['game_name'] = df['game_name'].apply(lambda x: game_rename(x))
 #     ax = sns.heatmap(df1.corr(), mask=mask, vmax=0.2, square=True, annot=True, fmt=".3f", cmap=cmap)
 
 # plt.yticks(rotation=0)
-# plt.xticks(rotation=0) 
+# plt.xticks(rotation=10) 
+# plt.tight_layout()
 # plt.show()
 
-# fig, ax = plt.subplots(1,1, figsize=(12,5))
-# sns.regplot(x="game_name", y="current_views", data=df1.loc[df1.follower_number <= 10000])
-# plt.show()
+# # fig, ax = plt.subplots(1,1, figsize=(12,5))
+# # sns.regplot(x="game_name", y="current_views", data=df1.loc[df1.follower_number <= 10000])
+# # plt.show()
 
-follower = df['current_views'].values
-print(max(follower))
-print(min(follower))
+# follower = df['current_views'].values
+# print(max(follower))
+# print(min(follower))
 
 def follower_group(follower):
     if follower >= 500000:
@@ -152,133 +160,79 @@ dfh['create_group'] = dfh['broadcasters_created_time'].apply(lambda x: create_gr
 # print(dfh[:5])
 
 # dfh = dfh.loc[dfh['game_name'].isin(["Dying Light","League of Legends","Grand Theft Auto V","StarCraft II: Heart of the Swarm","World of Warcraft: Warlords of Draenor","Dota 2","Counter-Strike: Global Offensive","Hearthstone"])]
-dfh = dfh.loc[dfh['game_name'].isin(["Dying Light","LOL","GTA V","StarCraft II","WOW","Dota 2","CS: GO","Hearthstone"])]
+dfh = dfh.loc[dfh['game_name'].isin(["Dying Light","LOL","GTA V","Destiny","WOW","COD:AW","CS: GO","Minecraft"])]
 
-# def in_top(x):
-#     if x in pack:
-#         return x
-#     else:
-#         pass
-
-# cols = ['follower_group', 'create_group']
-# for col in cols:
-#     pack = []
-#     dfh['Name'] = dfh[col]
-#     top = dfh[['Name', col]].groupby([col]).count().sort_values('Name', ascending=False).reset_index()
-#     for x in top[col]:
-#         pack.append(x)
-#     dfh[col] = dfh[col].apply(lambda x: in_top(x))
-#     dfh_platform = dfh[[col, 'game_name', 'current_views']].groupby([col, 'game_name']).mean().reset_index().pivot(col, 'game_name', 'current_views')
-#     plt.figure(figsize=(8, 5))
-#     sns.heatmap(dfh_platform, annot=True, fmt=".2g", linewidths=.5).set_title((' \n'+col+' vs. game name (by mean views) \n'))
-#     plt.ylabel('')
-#     plt.xlabel('Game Name \n')
-#     pack = []
-#     plt.yticks(rotation=0)
-#     plt.xticks(rotation=45)
-#     plt.tight_layout()
-#     plt.show()
-
-
-dfb = df[['game_name', 'language', 'broadcasters_created_time','current_views','follower_number']]
-dfb = dfb.dropna().reset_index(drop=True)
-df2 = dfb[['game_name', 'language', 'broadcasters_created_time','current_views','follower_number']]
-df2['Hit'] = df2['current_views']
-df2.drop('current_views', axis=1, inplace=True)
-
-def hit(view):
-    if view >= 1000:
-        return 1
+def in_top(x):
+    if x in pack:
+        return x
     else:
-        return 0
+        pass
 
-df2['Hit'] = df2['Hit'].apply(lambda x: hit(x))
-
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.calibration import CalibratedClassifierCV
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import StratifiedKFold
-from sklearn.feature_selection import SelectFromModel
-from sklearn.linear_model import LogisticRegression
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import classification_report, f1_score, accuracy_score, confusion_matrix
-from sklearn import svm
-
-from pandas import get_dummies
-df_copy = pd.get_dummies(df2)
-
-df3 = df_copy
-y = df3['Hit'].values
-df3 = df3.drop(['Hit'],axis=1)
-X = df3.values
-
-Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.50, random_state=2)
+cols = ['follower_group', 'create_group']
+for col in cols:
+    pack = []
+    dfh['Name'] = dfh[col]
+    top = dfh[['Name', col]].groupby([col]).count().sort_values('Name', ascending=False).reset_index()
+    for x in top[col]:
+        pack.append(x)
+    dfh[col] = dfh[col].apply(lambda x: in_top(x))
+    dfh_platform = dfh[[col, 'game_name', 'current_views']].groupby([col, 'game_name']).mean().reset_index().pivot(col, 'game_name', 'current_views')
+    plt.figure(figsize=(8, 5))
+    sns.heatmap(dfh_platform, annot=True, fmt=".2g", linewidths=.5).set_title((' \n'+col+' vs. game name (by mean views) \n'))
+    plt.ylabel('')
+    plt.xlabel('Game Name \n')
+    pack = []
+    plt.yticks(rotation=0)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
 
 
-#####################
-radm = RandomForestClassifier()
-parameter_space = { 
-    'n_estimators': [200, 500, 700],
-    'max_features': ['auto', 'sqrt', 'log2']
-}
+# dfb = df[['game_name', 'language', 'broadcasters_created_time','current_views','follower_number']]
+# dfb = dfb.dropna().reset_index(drop=True)
+# df2 = dfb[['game_name', 'language', 'broadcasters_created_time','current_views','follower_number']]
+# df2['Hit'] = df2['current_views']
+# df2.drop('current_views', axis=1, inplace=True)
 
-clf = GridSearchCV(radm, parameter_space, n_jobs=-1, cv=3)
-clf.fit(Xtrain, ytrain)
+# def hit(view):
+#     if view >= 1000:
+#         return 1
+#     else:
+#         return 0
 
-# Best paramete set
-print('Best parameters found:\n', clf.best_params_)
+# df2['Hit'] = df2['Hit'].apply(lambda x: hit(x))
 
-# All results
-means = clf.cv_results_['mean_test_score']
-stds = clf.cv_results_['std_test_score']
-for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-    print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
+# from sklearn.preprocessing import LabelEncoder
+# from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.calibration import CalibratedClassifierCV
+# from sklearn.model_selection import GridSearchCV
+# from sklearn.model_selection import StratifiedKFold
+# from sklearn.feature_selection import SelectFromModel
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.neural_network import MLPClassifier
+# from sklearn.metrics import classification_report, f1_score, accuracy_score, confusion_matrix
+# from sklearn import svm
 
-y_true, y_pred = ytest , clf.predict(Xtest)
+# from pandas import get_dummies
+# df_copy = pd.get_dummies(df2)
 
-print('Results on the test set:')
-print(classification_report(y_true, y_pred))
+# df3 = df_copy
+# y = df3['Hit'].values
+# df3 = df3.drop(['Hit'],axis=1)
+# X = df3.values
+
+# Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.50, random_state=2)
 
 
-######################
-
-# radm = RandomForestClassifier(random_state=2).fit(Xtrain, ytrain)
-# y_val_1 = radm.predict_proba(Xtest)
-# print("Validation accuracy: ", sum(pd.DataFrame(y_val_1).idxmax(axis=1).values
-#                                    == ytest)/len(ytest))
-
-# all_predictions = radm.predict(Xtest)
-# print(classification_report(ytest, all_predictions))
-
-# indices = np.argsort(radm.feature_importances_)[::-1]
-
-# # Print the feature ranking
-# print('Feature ranking (top 10):')
-
-# for f in range(10):
-#     print('%d. feature %d %s (%f)' % (f+1 , indices[f], df3.columns[indices[f]],
-#                                       radm.feature_importances_[indices[f]]))
-
-# svm_reg = svm.SVC(C=1.0,probability=True).fit(Xtrain, ytrain)
-# y_val_2 = svm_reg.predict_proba(Xtest)
-# print("Validation accuracy: ", sum(pd.DataFrame(y_val_2).idxmax(axis=1).values
-#                                    == ytest)/len(ytest))
-
-# all_predictions2 = svm_reg.predict(Xtest)
-# print(classification_report(ytest, all_predictions2))
-
-# mlp = MLPClassifier(max_iter=100)
-# parameter_space = {
-#     'hidden_layer_sizes': [(50,50,50), (50,100,50), (100,)],
-#     'activation': ['tanh', 'relu'],
-#     'solver': ['sgd', 'adam'],
-#     'alpha': [0.0001, 0.05],
-#     'learning_rate': ['constant','adaptive'],
+# #####################
+# radm = RandomForestClassifier()
+# parameter_space = { 
+#     'n_estimators': [200, 500, 700],
+#     'max_features': ['auto', 'sqrt', 'log2']
 # }
 
-# clf = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=3)
+# clf = GridSearchCV(radm, parameter_space, n_jobs=-1, cv=3)
 # clf.fit(Xtrain, ytrain)
 
 # # Best paramete set
@@ -295,10 +249,64 @@ print(classification_report(y_true, y_pred))
 # print('Results on the test set:')
 # print(classification_report(y_true, y_pred))
 
-# svm_reg = MLPClassifier(C=1.0,probability=True).fit(Xtrain, ytrain)
-# y_val_2 = svm_reg.predict_proba(Xtest)
-# print("Validation accuracy: ", sum(pd.DataFrame(y_val_2).idxmax(axis=1).values
-#                                    == ytest)/len(ytest))
 
-# all_predictions2 = svm_reg.predict(Xtest)
-# print(classification_report(ytest, all_predictions2))
+# ######################
+
+# # radm = RandomForestClassifier(random_state=2).fit(Xtrain, ytrain)
+# # y_val_1 = radm.predict_proba(Xtest)
+# # print("Validation accuracy: ", sum(pd.DataFrame(y_val_1).idxmax(axis=1).values
+# #                                    == ytest)/len(ytest))
+
+# # all_predictions = radm.predict(Xtest)
+# # print(classification_report(ytest, all_predictions))
+
+# # indices = np.argsort(radm.feature_importances_)[::-1]
+
+# # # Print the feature ranking
+# # print('Feature ranking (top 10):')
+
+# # for f in range(10):
+# #     print('%d. feature %d %s (%f)' % (f+1 , indices[f], df3.columns[indices[f]],
+# #                                       radm.feature_importances_[indices[f]]))
+
+# # svm_reg = svm.SVC(C=1.0,probability=True).fit(Xtrain, ytrain)
+# # y_val_2 = svm_reg.predict_proba(Xtest)
+# # print("Validation accuracy: ", sum(pd.DataFrame(y_val_2).idxmax(axis=1).values
+# #                                    == ytest)/len(ytest))
+
+# # all_predictions2 = svm_reg.predict(Xtest)
+# # print(classification_report(ytest, all_predictions2))
+
+# # mlp = MLPClassifier(max_iter=100)
+# # parameter_space = {
+# #     'hidden_layer_sizes': [(50,50,50), (50,100,50), (100,)],
+# #     'activation': ['tanh', 'relu'],
+# #     'solver': ['sgd', 'adam'],
+# #     'alpha': [0.0001, 0.05],
+# #     'learning_rate': ['constant','adaptive'],
+# # }
+
+# # clf = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=3)
+# # clf.fit(Xtrain, ytrain)
+
+# # # Best paramete set
+# # print('Best parameters found:\n', clf.best_params_)
+
+# # # All results
+# # means = clf.cv_results_['mean_test_score']
+# # stds = clf.cv_results_['std_test_score']
+# # for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+# #     print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
+
+# # y_true, y_pred = ytest , clf.predict(Xtest)
+
+# # print('Results on the test set:')
+# # print(classification_report(y_true, y_pred))
+
+# # svm_reg = MLPClassifier(C=1.0,probability=True).fit(Xtrain, ytrain)
+# # y_val_2 = svm_reg.predict_proba(Xtest)
+# # print("Validation accuracy: ", sum(pd.DataFrame(y_val_2).idxmax(axis=1).values
+# #                                    == ytest)/len(ytest))
+
+# # all_predictions2 = svm_reg.predict(Xtest)
+# # print(classification_report(ytest, all_predictions2))
